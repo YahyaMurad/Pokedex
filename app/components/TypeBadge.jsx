25a98315c2f7253298bdc2eee36fa50c5f86830e
Yahya Murad <yahyamurad197@gmail.com>
2024-04-24 03:32:42 +0800

    Commit

--- app/components/TypeBadge.jsx
+++ app/components/TypeBadge.jsx
@@ -1,9 +1,4 @@
-interface BadgeProps {
-  color: string;
-  content: string;
-}
-
-const Badge: React.FC<BadgeProps> = ({ color, content }) => {
+const Badge = ({ color, content }) => {
   return (
     <span className="badge badge-outline p-3 mr-3" style={{ color: color }}>
       {content}
